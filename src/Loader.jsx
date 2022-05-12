a6547496400023be89e4136df9be5d3781d7d30a
kunal232i <kingawale23@gmail.com>
2022-05-12 04:35:15 +0000

    Change

---
+++ src/Loader.jsx
@@ -0,0 +1,18 @@
+import "./Loader.css";
+import React from "react";
+const Loader = () => {
+  return (
+    <div class="lds-roller">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loader;
